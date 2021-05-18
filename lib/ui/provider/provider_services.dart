b3072c7f58b417b7e9cf28de7e3b82ea1014ef77
Kunal agarwal <58211537+kunala2004@users.noreply.github.com>
2021-05-19 00:44:06 +0530

    Bottom Navigation UI Sorted Size Issues Resolved

--- lib/ui/provider/provider_services.dart
+++ lib/ui/provider/provider_services.dart
@@ -3,6 +3,8 @@ import 'package:covigenix/ui/provider/edit.dart';
 import 'package:covigenix/ui/provider/provider_profile.dart';
 import 'package:covigenix/ui/provider/provider_requests.dart';
 import 'package:flutter/material.dart';
+import 'package:motion_tab_bar/MotionTabController.dart';
+import 'package:motion_tab_bar/motiontabbar.dart';
 
 /// This is the main application widget.
 
@@ -11,10 +13,12 @@ class ProviderServices extends StatefulWidget {
 
   @override
   _ProviderServicesState createState() => _ProviderServicesState();
+
 }
 
 /// This is the private State class that goes with MyStatefulWidget.
 class _ProviderServicesState extends State<ProviderServices> {
+  late MotionTabController _tabController;
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
   TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
@@ -30,6 +34,12 @@ class _ProviderServicesState extends State<ProviderServices> {
       _selectedIndex = index;
     });
   }
+  @override
+  void dispose() {
+    super.dispose();
+    _tabController.dispose();
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -37,30 +47,45 @@ class _ProviderServicesState extends State<ProviderServices> {
       body: Center(
         child: _widgetOptions.elementAt(_selectedIndex),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Requests',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Community',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'Essentials',
-            backgroundColor: Colors.black
-          ),
-          /*BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle_outlined),
-            label: 'Profile',
-          ),*/
+      // bottomNavigationBar: BottomNavigationBar(
+      //   type: BottomNavigationBarType.fixed,
+      //   items: const <BottomNavigationBarItem>[
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.home),
+      //       label: 'Requests',
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.business),
+      //       label: 'Community',
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.school),
+      //       label: 'Essentials',
+      //       backgroundColor: Colors.black
+      //     ),
+      //     /*BottomNavigationBarItem(
+      //       icon: Icon(Icons.account_circle_outlined),
+      //       label: 'Profile',
+      //     ),*/
+      //   ],
+      //   currentIndex: _selectedIndex,
+      //   selectedItemColor: Colors.amber[800],
+      //   onTap: _onItemTapped,
+      // ),
+      bottomNavigationBar: MotionTabBar(
+        labels: [
+          "Home","Community","My Requests"
         ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
+        initialSelectedTab: "Home",
+        tabIconColor: Colors.blue,
+        tabSelectedColor: Colors.yellow,
+        onTabItemSelected: (int value){
+               _onItemTapped(value);
+        },
+        icons: [
+          Icons.home,Icons.business,Icons.school
+        ],
+        textStyle: TextStyle(color: Colors.red),
       ),
     );
   }
